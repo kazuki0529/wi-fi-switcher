4c44da3a02ab2944c95110d23d5a592dfe6873ad
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2021-09-20 15:26:10 +0000

    処理方式の修正

--- cypress/integration/PR-500KI-wifi-off.ts
+++ cypress/integration/PR-500KI-wifi-off.ts
@@ -1,12 +1,18 @@
 /// <reference types="cypress" />
 
 it('Wi-Fi Off', () => {
-  cy.visit('http://ntt.setup/ntt/wireless/fifth/common2-4ghz/', {
+  cy.visit('http://ntt.setup/', {
     auth: {
       username: 'user',
       password: 'tod2judas',
     },
   });
-  cy.get('#ssidFlag').uncheck();
-  cy.get('#SETTING_BUTTON').click();
+  cy.visit('http://ntt.setup/ntt/wireless/fifth/common', {
+    auth: {
+      username: 'user',
+      password: 'tod2judas',
+    },
+  });
+  cy.get('#WLAN_AP_SETTING_2_4GHz_CHECK').uncheck();
+  cy.get('#WLAN_AP_SETTING_SET').click();
 });
--- cypress/integration/PR-500KI-wifi-on.ts
+++ cypress/integration/PR-500KI-wifi-on.ts
@@ -1,12 +1,18 @@
 /// <reference types="cypress" />
 
 it('Wi-Fi On', () => {
-  cy.visit('http://ntt.setup/ntt/wireless/fifth/common2-4ghz/', {
+  cy.visit('http://ntt.setup/', {
     auth: {
       username: 'user',
       password: 'tod2judas',
     },
   });
-  cy.get('#ssidFlag').check();
-  cy.get('#SETTING_BUTTON').click();
+  cy.visit('http://ntt.setup/ntt/wireless/fifth/common', {
+    auth: {
+      username: 'user',
+      password: 'tod2judas',
+    },
+  });
+  cy.get('#WLAN_AP_SETTING_2_4GHz_CHECK').check();
+  cy.get('#WLAN_AP_SETTING_SET').click();
 });
