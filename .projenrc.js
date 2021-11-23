8bfa3e35ea4fddb54f5ff797120c18b26f8575ce
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2021-11-24 00:47:36 +0900

    #36 bug fixed

--- .projenrc.js
+++ .projenrc.js
@@ -16,6 +16,18 @@ const project = new AwsCdkTypeScriptApp({
   context: {
     '@aws-cdk/core:newStyleStackSynthesis': true,
   },
+  pullRequestTemplateContents: [
+    '## 解決するチケット',
+    '',
+    'fixed #xx',
+    '',
+    '## 修正内容',
+    '',
+    '',
+    '## その他',
+    '',
+    '- レビュワーへの参考情報（実装上の懸念点や注意点などあれば記載）',
+  ],
   // deps: [],                    /* Runtime dependencies of this module. */
   // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                 /* Build dependencies for this module. */
